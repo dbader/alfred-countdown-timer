69e65bfaf8ad71b8a70ff107203a23a37609f9ef
Daniel Bader <mail@dbader.org>
2013-01-17 17:29:06 +0100

    Initial code commit.

---
+++ Timer/alarm.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+"""
+A simple countdown timer command for Alfred.app that
+uses Mountain Lion User Notifications.
+
+Copyright (c) 2013 Daniel Bader (http://dbader.org)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import sys
+import time
+import subprocess
+import objc
+
+def main():
+    minutes = read_time()
+    notify('Alarm set to %i %s' % (minutes, 'minute' if minutes == 1 else 'minutes'))
+
+    time.sleep(minutes * 60)
+
+    notify("Time's up!", '%i %s passed.' % (minutes, 'minute has' if minutes == 1 else 'minutes have'))
+    play_sound('alarm.m4a')
+
+def read_time():
+    """Parse the desired countdown time from the commandline."""
+    try:
+        return float(sys.argv[1])
+    except:
+        show_usage()
+        sys.exit(1)
+
+def show_usage():
+    notify('Alarm usage', 'alarm [minutes]')
+
+def notify(title, subtitle=None):
+    """Display a NSUserNotification on Mac OS X >= 10.8"""
+    NSUserNotification = objc.lookUpClass('NSUserNotification')
+    NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
+    if not NSUserNotification or not NSUserNotificationCenter:
+        return
+
+    notification = NSUserNotification.alloc().init()
+    notification.setTitle_(str(title))
+    if subtitle:
+        notification.setSubtitle_(str(subtitle))
+
+    NSUserNotificationCenter.defaultUserNotificationCenter().scheduleNotification_(notification)
+
+def play_sound(filename):
+    subprocess.Popen(['afplay', filename])
+
+if __name__ == '__main__':
+    main()
